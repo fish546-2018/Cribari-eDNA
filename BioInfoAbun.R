0cc21cec1c4b0634ff9a122144138302edf9143a
Kelly Cribari <kellycribari@D-10-19-243-191.dhcp4.washington.edu>
2018-11-27 15:27:25 -0800

    Abun2

--- BioInfoAbun.R
+++ BioInfoAbun.R
@@ -3,8 +3,10 @@ all.results.files<-list.files(path="/Users/kellycribari/Documents/Bioinformatics
 all.results.names <-list.files(path="/Users/kellycribari/Documents/Bioinformatics/GitHub/Cribari-eDNA/Analysis/")
 all.results.dfs <- map(all.results.files, function(x) { read_delim(file = x,delim = "\t",col_names = F) })
 names(all.results.dfs)<- all.results.names
+
 sample1 <- read_delim(file = "/Users/kellycribari/Documents/Bioinformatics/GitHub/Cribari-eDNA/Analysis/sampleACACAC", delim = "\t", col_names = F )
 sample1$X2
+
 all.in.one.df <- bind_rows(all.results.dfs, .id = "Sample")
 my_wrapper<- function(df){
   
@@ -27,3 +29,4 @@ all.in.one.df %>%
   mutate(Size = as.numeric(str_replace(Size, ";", ""))) %>% 
   group_by(Sample,Match) %>% 
   dplyr::summarise(nReads = sum(Size)) -> FinalAbun
+
