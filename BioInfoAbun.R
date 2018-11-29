2591d68397db399069b894b55712bad0468f1793
Kelly Cribari <kellycribari@D-10-19-207-28.dhcp4.washington.edu>
2018-11-29 10:16:02 -0800

    Update BioInfoAbun.R

--- BioInfoAbun.R
+++ BioInfoAbun.R
@@ -11,11 +11,11 @@ all.in.one.df <- bind_rows(all.results.dfs, .id = "Sample")
 all.in.one.df %>%
   filter( X3 > 95 ) %>% #Match more than 95%
   filter( X4 > 285) %>% #match size greater than 285 bp 
-  separate(X1, into = c("Shit", "Size" ), sep = ";size=") %>% #Separate columns to isolate the size of the match
+  separate(X1, into = c("Shit", "Size" ), sep = ";size=") %>% #Separate columns to isolate the number of matches
   separate(X2, into = c("moreShit", "Match"), sep = ";") %>% #Separate columns to isolate name of the taxa
   mutate(Size = as.numeric(str_replace(Size, ";", ""))) %>% #Remove excess characters to make size a numeric variable
   group_by(Sample,Match) %>% 
-  dplyr::summarise(nReads = sum(Size)) -> FinalAbun 
+  dplyr::summarise(nReads = sum(Size)) -> FinalAbun #Size is the number of matches found, this is summing the total number of matches per taxa
 
 ggplot(data = FinalAbun, aes(x=Match, y=Sample, fill=nReads))+
   geom_bin2d()+
