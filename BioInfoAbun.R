9813a916374df94f9e1da815bbcc2efd2d5e78fc
Kelly Cribari <kellycribari@D-10-19-207-28.dhcp4.washington.edu>
2018-11-29 09:42:06 -0800

    Update R Script

--- BioInfoAbun.R
+++ BioInfoAbun.R
@@ -1,38 +1,25 @@
 library (tidyverse)
+#load all files
 all.results.files<-list.files(path="/Users/kellycribari/Documents/Bioinformatics/GitHub/Cribari-eDNA/Analysis/",full.names = T)
 all.results.names <-list.files(path="/Users/kellycribari/Documents/Bioinformatics/GitHub/Cribari-eDNA/Analysis/")
 all.results.dfs <- map(all.results.files, function(x) { read_delim(file = x,delim = "\t",col_names = F) })
 names(all.results.dfs)<- all.results.names
 
-sample1 <- read_delim(file = "/Users/kellycribari/Documents/Bioinformatics/GitHub/Cribari-eDNA/Analysis/sampleACACAC", delim = "\t", col_names = F )
-sample1$X2
-
+#Put into one dataframe
 all.in.one.df <- bind_rows(all.results.dfs, .id = "Sample")
-my_wrapper<- function(df){
-  
-  df %>% 
-    filter( X3 > 95 ) %>%
-    filter( X4 > 285) %>% #size greater than 285 needs to meet both
-    separate(X1, into = c("Shit", "Size" ), sep = ";size=") %>% 
-    separate(X2, into = c("moreShit", "Match"), sep = ";") %>% 
-    mutate(Size = as.numeric(str_replace(Size, ";", ""))) %>% 
-    group_by(Match) %>% 
-    dplyr::summarise(nReads = sum(Size))
-}
-
 
 all.in.one.df %>%
-  filter( X3 > 95 ) %>%
-  filter( X4 > 285) %>% #size greater than 285 needs to meet both
-  separate(X1, into = c("Shit", "Size" ), sep = ";size=") %>% 
-  separate(X2, into = c("moreShit", "Match"), sep = ";") %>% 
-  mutate(Size = as.numeric(str_replace(Size, ";", ""))) %>% 
+  filter( X3 > 95 ) %>% #Match more than 95%
+  filter( X4 > 285) %>% #match size greater than 285 bp 
+  separate(X1, into = c("Shit", "Size" ), sep = ";size=") %>% #Separate columns to isolate the size of the match
+  separate(X2, into = c("moreShit", "Match"), sep = ";") %>% #Separate columns to isolate name of the taxa
+  mutate(Size = as.numeric(str_replace(Size, ";", ""))) %>% #Remove excess characters to make size a numeric variable
   group_by(Sample,Match) %>% 
-  dplyr::summarise(nReads = sum(Size)) -> FinalAbun
+  dplyr::summarise(nReads = sum(Size)) -> FinalAbun 
 
 ggplot(data = FinalAbun, aes(x=Match, y=Sample, fill=nReads))+
   geom_bin2d()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
+#Heat map of matches by sample (location) with a color indicating the number of reads for that match
 
 
